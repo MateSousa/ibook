1b8f9790048b3a58fd510e369147a9765aeac2d5
Matheus Sousa <matessousa@outlook.com>
2021-11-24 18:34:48 -0300

    first

---
+++ components/organisms/BookList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="book-list">
+        <BookCard v-for="book in $books" :key="book.id"  />
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+    import { books } from '@/store'
+
+    export default Vue.extend({
+        computed: {
+            $books() {
+                return books.$all
+            }
+        }
+    })
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ components/organisms/Presentation.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="presentation">
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis labore voluptates rerum reprehenderit dolorum sequi aliquid voluptatum saepe excepturi vel modi mollitia neque accusantium aperiam laborum velit sunt, repellat illo?
+        </p>
+
+        <img src="@/assets/img/presentation.svg" alt="Presentation of iBook">
+    </div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+
+    export default Vue.extend({
+        
+    
+    })
+</script>
+
+<style lang="scss" scoped>
+
+.presentation {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 2rem;
+  align-items: center;
+  @include screen('small', 'medium') {
+    grid-template-columns: 1fr;
+  }
+  p {
+    line-height: 26px;
+  }
+  img {
+    max-width: 100%;
+  }
+}
+
+</style>
